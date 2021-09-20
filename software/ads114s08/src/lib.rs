4af9db0196c8c7c2a91180eb5103ea280f969137
Lachlan Sneff <lachlan.sneff@gmail.com>
2021-09-20 13:09:34 -0400

    Add read function to ADS114S08 driver

--- software/ads114s08/src/lib.rs
+++ software/ads114s08/src/lib.rs
@@ -13,11 +13,39 @@ use embedded_hal::{
 mod consts;
 use self::consts::*;
 
+struct CsGuard<'a, CS: OutputPin<Error = Infallible>>(&'a mut CS);
+
+impl<'a, CS: OutputPin<Error = Infallible>> CsGuard<'a, CS> {
+    fn new(cs: &'a mut CS) -> Self {
+        let _ = cs.set_low();
+        Self(cs)
+    }
+}
+
+impl<'a, CS: OutputPin<Error = Infallible>> Drop for CsGuard<'a, CS> {
+    fn drop(&mut self) {
+        let _ = self.0.set_high();
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Error<E> {
     InvalidDevice,
     InvalidDeviceState,
     SpiError(E),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Gain {
+    X1 = 0b000,
+    X2 = 0b001,
+    X4 = 0b010,
+    X8 = 0b011,
+    X16 = 0b100,
+    X32 = 0b101,
+    X64 = 0b110,
+    X128 = 0b111,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -77,54 +105,60 @@ where
         Ok(this)
     }
 
-    pub fn read(&mut self, inputs: [Channel; 2]) -> Result<u16, Error<E>> {
+    pub fn read(&mut self, low: Channel, high: Channel, gain: Gain) -> Result<i16, Error<E>> {
         while let Some(false) = self.ready.is_low().ok() {}
-        let _ = self.cs.set_low();
+        let mut guard = CsGuard::new(&mut self.cs);
+
+        // Set the channels that are read from.
+        let inpmux = (low as u8) | ((high as u8) << 4);
+        let gain = gain as u8 | 0b01 << 4;
+
+        Self::write_regs(&mut self.spi, &mut guard, Reg::INPMUX, [inpmux, gain])?;
+
+        // Get the reading from the device
+        self.spi.write(&[cmd::RDATA])?;
+        let mut data = [0; 2];
+        self.spi.transfer(&mut data)?;
+
+        Ok(i16::from_le_bytes(data))
     }
 
     fn init(&mut self) -> Result<(), Error<E>> {
-        let _ = self.cs.set_low();
+        let mut guard = CsGuard::new(&mut self.cs);
 
-        self.reset()?;
+        // Reset the device
+        self.spi.write(&[cmd::RESET])?;
+        self.delay.delay_ms(1);
 
         // Check if the RDY bit is 0.
-        let [status] = self.read_regs(Reg::STATUS)?;
+        let [status] = Self::read_regs(&mut self.spi, &mut guard, Reg::STATUS)?;
         if status & (1 << 6) != 0 {
             return Err(Error::InvalidDevice);
         }
 
         // Clear the FL_POR flag
-        self.write_regs(Reg::STATUS, [0])?;
+        Self::write_regs(&mut self.spi, &mut guard, Reg::STATUS, [0])?;
         
         self.spi.write(&[cmd::START])?;
 
-        let _ = self.cs.set_high();
-
         Ok(())
     }
 
-    /// Resets the device and blocks for 1ms.
-    fn reset(&mut self) -> Result<(), Error<E>> {
-        self.spi.write(&[cmd::RESET])?;
-        self.delay.delay_ms(1);
-        Ok(())
-    }
-
-    fn read_regs<const N: usize>(&mut self, start_reg: Reg) -> Result<[u8; N], Error<E>> {
+    fn read_regs<const N: usize>(spi: &mut SPI, _: &mut CsGuard<CS>, start_reg: Reg) -> Result<[u8; N], Error<E>> {
         assert!(N <= 0b1_1111);
 
-        self.spi.write(&cmd::rreg(start_reg as u8, N as u8))?;
+        spi.write(&cmd::rreg(start_reg as u8, N as u8))?;
         let mut output = [0; N];
-        self.spi.transfer(&mut output)?;
-        
+        spi.transfer(&mut output)?;
+
         Ok(output)
     }
 
-    fn write_regs<const N: usize>(&mut self, start_reg: Reg, data: [u8; N]) -> Result<(), Error<E>> {
+    fn write_regs<const N: usize>(spi: &mut SPI,  _: &mut CsGuard<CS>, start_reg: Reg, data: [u8; N]) -> Result<(), Error<E>> {
         assert!(N <= 0b1_1111);
 
-        self.spi.write(&cmd::wreg(start_reg as u8, N as u8))?;
-        self.spi.write(&data)?;
+        spi.write(&cmd::wreg(start_reg as u8, N as u8))?;
+        spi.write(&data)?;
 
         Ok(())
     }
